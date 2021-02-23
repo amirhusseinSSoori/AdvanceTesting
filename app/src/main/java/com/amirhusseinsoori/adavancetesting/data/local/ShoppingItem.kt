86300cb18974f9a3b2bd19637534aa969c41a261
amirhussein soori <amirhusseinsoori75@gmail.com>
2021-02-23 17:17:33 +0330

    add library

---
+++ app/src/main/java/com/amirhusseinsoori/adavancetesting/data/local/ShoppingItem.kt
@@ -0,0 +1,13 @@
+package com.amirhusseinsoori.adavancetesting.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity(tableName = "shopping_items")
+data class ShoppingItem(
+    var name: String,
+    var amount: String,
+    var price: Float,
+    var imageUrl: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null
+)
